4926cc3750c2078a18d0c4371c70d8f465823fd2
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-06-29 23:27:52 +0530

    createTransaction

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require("express")
 const authMiddleware = require('../middleware/auth.middleware')
+const transactioncontroller = require('../controllers/transaction.controller')
 
 
 const transaction = Router()
 
-transactionRouter.post("/", authMiddleware.authMiddleware)
+transactionRouter.post("/", authMiddleware.authMiddleware, transactioncontroller.createTransaction)
 
 
 module.exports = transactionRouter;
