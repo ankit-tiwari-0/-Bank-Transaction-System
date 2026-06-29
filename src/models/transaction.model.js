80c3c17addac2922dfe470d900bd944451c342b8
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-06-29 21:40:53 +0530

    add transaction.model

---
+++ src/models/transaction.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+
+const transationSchema = new mongoose.Schema({
+
+    fromAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [true, "Transaction must be associated with a from account"],
+        index: true
+    },
+
+    toAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [true, "Transaction must be associated with a to account"],
+        index: true
+    },
+    status:{
+        type: String,
+        enum:{
+            value: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+            message: "Status can be either PENDING, COMPLETED, FAILED OR REVERSED",
+        },
+        default: "PENDING"
+    },
+    amount: {
+        type: Number,
+        required: [true, "Amount is required for creating a transaction"],
+        min:[0, "Transaction amount cannot be negativeqwq"]
+    },
+    idempoteacyKey: {
+        type: String,
+        required: [true],
+        index: true,
+        unique: true
+    }
+
+}{
+    timestamps: true
+})
+
+const transactionModel = mongoose.model("transaction", transationSchema)
