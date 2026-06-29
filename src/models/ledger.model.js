a4daf8a64ade583323f3cec9de891eb4551d9808
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-06-29 22:05:26 +0530

    git addd leger model

---
+++ src/models/ledger.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose")
+
+
+const ledgerSchema = new mongoose.Schema({
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "account",
+    required: [true, "Ledger must be associated with an account"],
+    index: true,
+    immutable: true,
+  },
+
+  amount: {
+    type: Number,
+    required: [true, "Amount is required for creating a ledger entry"],
+    immutable: true,
+  },
+
+  transaction: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "transaction",
+    required: [true, "Ledger must be associated with a transaction"],
+    index: true,
+    immutable: true,
+  },
+  type:{
+    type: String,
+    enum: {
+        values: ["CREADIT", "DEBIT"],
+        message: "Type can be either Creadit or Debit",
+    },
+    required: [true],
+    immutable: true
+  }
+});
+
+function preventLedgerModification() {
+    throw new Error("Ledger entries are imutable and cannot be modified or delete");
+}
+
+
+ledgerSchema.pre('findoneAndUpdate', preventLedgerModification);
+ledgerSchema.pre('updateone', preventLedgerModification);
+ledgerSchema.pre('deleteone', preventLedgerModification);
+ledgerSchema.pre('remove', preventLedgerModification);
+ledgerSchema.pre('deleteMany', preventLedgerModification);
+
+const legerModel = mongoose.model('leger',ledgerSchema)
